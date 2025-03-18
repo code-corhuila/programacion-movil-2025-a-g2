f96882de9473d46e77822f73e9cbdfe7bd04bcfa
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2025-03-18 10:09:38 -0500

    feature[HU-06] ✨ Structure project - backend

---
+++ backend/shoppind-car/src/main/java/com/corhuila/shoppind_car/ShoppindCarApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.shoppind_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppindCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppindCarApplication.class, args);
+	}
+
+}
---
+++ backend/shoppind-car/src/test/java/com/corhuila/shoppind_car/ShoppindCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.shoppind_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppindCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
